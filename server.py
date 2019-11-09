5385b8b3e27b47e5a01c96e76a335edcaf862f11
Harnet69 <harnet69@gmail.com>
2019-11-09 19:53:14 +0100

    Added PUT and DELETE methods, used global dictionary instead variabbles

--- server.py
+++ server.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-METHOD_GET = 0
-METHOD_POST = 0
+requests = {"METHOD_GET":0, "METHOD_POST":0, "METHOD_PUT":0, "METHOD_DELETE":0}
 
 
 @app.route('/')
@@ -14,19 +13,19 @@ def index_route():
 @app.route('/request-counter', methods=["GET", "POST"])
 def request_counter_route():
     if request.method == "GET":
-        print("GET")
-        global METHOD_GET
-        METHOD_GET += 1
+        requests['METHOD_GET'] += 1
     if request.method == "POST":
-        print("POST")
-        global METHOD_POST
-        METHOD_POST += 1
-    return render_template("request-counter.html", method_get=METHOD_GET, method_post=METHOD_POST)
+        requests['METHOD_POST'] += 1
+    if request.method == "PUT":
+        requests['METHOD_PUT'] += 1
+    if request.method == "DELETE":
+        requests['METHOD_DELETE'] += 1
+    return render_template("request-counter.html", requests=requests)
 
 
 @app.route('/statistics')
 def statistics_route():
-    return render_template("statistics.html")
+    return render_template("statistics.html", requests=requests)
 
 
 if __name__ == '__main__':
